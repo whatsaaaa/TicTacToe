9b602280f31f92b0ce222915023eab65878893aa
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-02-22 01:07:35 +0100

    Remove async

--- server/src/graphql/resolvers/SubscriptionResolver.ts
+++ server/src/graphql/resolvers/SubscriptionResolver.ts
@@ -9,7 +9,7 @@ export default class SubscriptionResolver {
   @Subscription(type => String, {
     topics: "GAME_ENDED"
   })
-  async liveResults(@Ctx() ctx: any, @Root() liveResult: IGameEnded): Promise<string> {
+  liveResults(@Ctx() ctx: any, @Root() liveResult: IGameEnded): string {
     const winner = liveResult.winner;
     const text = winner == "draw" ? "draw" : `${winner} won.`;
     return `Game ${liveResult.gameId} finished. Result is: ${text}. Board: ${JSON.stringify(liveResult.board)}`;
